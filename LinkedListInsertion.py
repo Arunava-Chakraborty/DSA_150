3192cc798f05c1fa6ca52a49d83f705f82236a7f
Arunava Chakraborty <118994880+Arunava-Chakraborty@users.noreply.github.com>
2024-08-24 09:57:11 +0530

    Update LinkedListInsertion.py

--- LinkedListInsertion.py
+++ LinkedListInsertion.py
@@ -5,25 +5,21 @@ class Node:
     # This will create a node and just the node only and the reference value will be none then we create a class for linking the nodes.
 
 class LinkedList:
-  
     def __init__(self):
         self.head = None
-        self.temp = None
-      
+
     def print_LL(self):
         if self.head is None:
             print("The linked list is empty")
         else:
             n = self.head
             while n is not None:
-                print(n.data)
+                print(n.data , "--->" , end=" ")
                 n = n.ref
-              
     def add_begin(self , data):
         new_node = Node(data)
         new_node.ref = self.head
         self.head =new_node
-      
     def add_end(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -33,7 +29,6 @@ class LinkedList:
             while temp.ref is not None:
                 temp= temp.ref
             temp.ref = new_node
-          
     def insert_after(self, data,x):
         n=self.head
         while n is not None:
@@ -46,7 +41,6 @@ class LinkedList:
             new_node = Node(data)
             new_node.ref = n.ref
             n.ref = new_node
-          
     def insert_before(self, data, x):
         if self.head == None:
             print("Linked List is null!")
@@ -68,16 +62,55 @@ class LinkedList:
                 new_node.ref = n.ref
                 n.ref =new_node
 
+    def delete_beg(self):
+        n= self.head
+        if self.head is None:
+            print("LL is Empty")
+            return
+        if n.ref is None:
+            self.head = None
+        else:
+            self.head = n.ref.ref
 
-#Test Cases
+    def deletion(self):
+        n = self.head
+        if self.head == None:
+            print("Linked List is null!")
+            return
+        if n.ref == None:
+            self.delete_beg()
+        while n.ref is not None:
+            if n.ref.ref ==None:
+                n.ref =None
+                break
+            n = n.ref
+    def delete_value(self , x):
+        n = self.head
+        if self.head == None:
+            print("LL is empty")
+            return
+        if self.head.data == x:
+            self.head = self.head.ref
+        else:
+            while n.ref is not None:
+                if n.ref.data == x:
+                    n.ref = n.ref.ref
+                    break
+                n=n.ref
 ll1 = LinkedList()
 ll1.add_end(10)
 ll1.add_end(20)
 ll1.add_end(40)
 ll1.add_end(50)
 ll1.add_end(60)
-ll1.insert_after(10 ,20)
-ll1.insert_before(80 , 10)
+ll1.delete_value(40)
+ll1.delete_value(20)
+ll1.delete_value(10)
+ll1.delete_value(50)
+ll1.delete_value(60)
+ll1.add_end(40)
+ll1.add_begin(70)
+
 ll1.print_LL()
 
 
